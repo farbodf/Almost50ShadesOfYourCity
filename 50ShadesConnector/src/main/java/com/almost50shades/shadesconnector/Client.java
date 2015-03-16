a6c8b240654a58a9b149929c601e710a3aaed095
martinradev <martin.b.radev@gmail.com>
2015-03-16 11:30:45 +0200

    Update Client.java

--- 50ShadesConnector/src/main/java/com/almost50shades/shadesconnector/Client.java
+++ 50ShadesConnector/src/main/java/com/almost50shades/shadesconnector/Client.java
@@ -1,8 +1,3 @@
-/*
- *  Copyright (c) Screenful Ltd 2014
- *  This file is subject to the terms and conditions defined in
- *  file 'LICENSE.txt', which is part of this source code package.
- */
 
 package com.almost50shades.shadesconnector;
 
