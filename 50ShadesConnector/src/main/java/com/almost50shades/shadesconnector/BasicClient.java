b06638e9afa1e9fd5df97861dc3666b4194689e6
martinradev <martin.b.radev@gmail.com>
2015-03-16 11:31:02 +0200

    Update BasicClient.java

--- 50ShadesConnector/src/main/java/com/almost50shades/shadesconnector/BasicClient.java
+++ 50ShadesConnector/src/main/java/com/almost50shades/shadesconnector/BasicClient.java
@@ -1,8 +1,4 @@
-/*
- *  Copyright (c) Screenful Ltd 2014
- *  This file is subject to the terms and conditions defined in
- *  file 'LICENSE.txt', which is part of this source code package.
- */
+
 
 package com.almost50shades.shadesconnector;
 
